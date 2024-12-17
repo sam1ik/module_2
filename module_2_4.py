c2617328bbbd7e92600ca2ad07d82fb03fa3c16d
sam1ik <samvelik277@gmail.com>
2024-12-17 12:52:18 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,19 @@
+# "Все не так уж просто"
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    is_prime = True
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+                break
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+primes.remove(primes[0])
+print(primes)
+print(not_primes)
